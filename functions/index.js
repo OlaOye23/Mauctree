b7c958298e5b26816005ff74ed88c89544a270ce
LaoluOye <ADA@Olaoluwapos-MacBook-Pro.local>
2020-11-05 09:54:54 +0100

    cloud functions

---
+++ functions/index.js
@@ -0,0 +1,36 @@
+const functions = require('firebase-functions');
+const fetch = require("node-fetch");
+
+
+
+
+exports.onNewOrder = functions.firestore
+  .document('/orders/{id}')
+  .onCreate((change, context) => {
+    console.log('context parameters')
+    console.log(context.params)
+    async function sendPushNotification(expoPushToken) {
+        const message = {
+          to: expoPushToken,
+          sound: 'default',
+          title: 'New Order!',
+          body: 'Open app to see details',
+          data: { name: context.params.name,
+                  address: context.params.address,
+                  total: context.params.total,  
+                },
+        };
+      
+        await fetch('https://exp.host/--/api/v2/push/send', {
+          method: 'POST',
+          headers: {
+            Accept: 'application/json',
+            'Accept-encoding': 'gzip, deflate',
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(message),
+        });
+      }
+    sendPushNotification("ExponentPushToken[CCn2edEVIjaUzfnqhSebut]") 
+    console.log('new order alert complete')
+});
